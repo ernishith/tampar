88a5aaa991ee6c683639674da68ce4f44e704f81
ernishith <er.nishith@gmail.com>
2026-02-08 06:42:21 -0500

    Revert "printing uvmaps path for debugging"
    This reverts commit e77230221731ad4bdc48736dd317729f78e88c21.

--- src/tools/create_uvmaps.py
+++ src/tools/create_uvmaps.py
@@ -246,7 +246,6 @@ if __name__ == "__main__":
             if (image_root / Path(img["file_name"])).exists()
         ]
         print(f"Processing {len(image_paths)} images...")
-        print(f"Sample image paths: {image_paths[:5]}")
         create_pred_uvmaps(predictor, image_paths)
 
     # Summary
